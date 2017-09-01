df548fc43a0448735f7272ccfb2ef2ec5bb587ca
Rodriguez, Juan Manuel <juarodriguez@grupoassa.com>
2017-09-01 18:01:53 -0300

    estructura practico 2 y drag and drop

--- TP2/js/p2-ej5-b.js
+++ TP2/js/p2-ej5-b.js
@@ -1,5 +1,8 @@
 var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+var ctx = canvas.getContext("2d"),
+	dragging = false,
+	x=0,
+	y=0;
 
 function colourRandom(){
 	var hexa='0123456789abcdef';
@@ -14,6 +17,9 @@ function positionRandom(max){
 	return Math.floor(Math.random()*max);
 }
 
+function borrar(){
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+}
 
 function Circulo(x,y,radio,color){
 	this.posx=x;
@@ -30,26 +36,6 @@ Circulo.prototype.dibujar= function(){
 	ctx.closePath();
 }
 
-Circulo.prototype.dibujarRandom = function(){
-	var hexa='0123456789abcdef';
-	
-	console.log(this.color);
-	var that=this;
-	setInterval(function(){ 
-		that.color='#';
-		for(var i=0;i<6;i++){
-			that.color+=hexa[Math.floor(Math.random()*16)]
-		}
-		that.posx=Math.floor(Math.random()*900);
-		that.posy=Math.floor(Math.random()*600);
-		ctx.fillStyle=that.color;
-		ctx.beginPath();
-		ctx.arc(that.posx,that.posy,that.radio,0,(Math.PI/180)*360,true);
-		ctx.fill();
-		ctx.closePath();
-	}, 1000);//cada 1 segs
-}
-
 Circulo.prototype.mousedown = function(clientx,clienty){
 	var x = Math.pow((clientx-this.posx),2);
 	var y = Math.pow((clienty-this.posy),2);
@@ -57,7 +43,7 @@ Circulo.prototype.mousedown = function(clientx,clienty){
 	if(d1 <= this.radio){
 		this.color=colourRandom();
 		console.log("click dentro del circulo. El color del circulo cambiara");
-		return
+		dragging = true;
 	}
 	else{
 		console.log("click fuera del circulo");
@@ -67,20 +53,22 @@ Circulo.prototype.mousedown = function(clientx,clienty){
 var color = colourRandom();
 var x = positionRandom(canvas.width);
 var y = positionRandom(canvas.height);
-var cir = new Circulo(x,y,40,color);
+var cir = new Circulo(x,y,75,color);
 
 ctx.onload = cir.dibujar();
 
 canvas.onmousedown = function(event){
-	/*canvas.onmousemove = function(e){
-		console.log("x "+e.clientX+" y "+e.clientY);
-		var cir2 = new Circulo(e.clientX,e.clientY,40,color);
-		cir.mousedown(event.clientX,event.clientY);
-		cir2.dibujar();
-	}*/
-	cir.mousedown(event.clientX,event.clientY);
-	cir.dibujar();
+	cir.mousedown(event.layerX,event.layerY);
+	if(dragging){
+		canvas.onmousemove = function(e){
+			cir.posx=e.layerX;
+			cir.posy=e.layerY;
+			borrar();
+			cir.dibujar();
+		}	
+	}
 }
 canvas.onmouseup = function (){
-	console.log("BASTA!!");
+	dragging=false;
+	canvas.onmousemove=null;
 }
