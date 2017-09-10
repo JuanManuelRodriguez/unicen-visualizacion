5781c3475f39347ec42e2153e70dd0758b698ce7
Rodriguez, Juan Manuel <juarodriguez@grupoassa.com>
2017-09-10 20:04:11 -0300

    drag and drop varias figuras

--- TP2/js/p2-ej5-b.js
+++ TP2/js/p2-ej5-b.js
@@ -1,6 +1,8 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d"),
-	dragging = false,
+	draggingCir = false,
+	draggingRec = false,
+	draggingReCua = false,
 	x=0,
 	y=0;
 
@@ -21,6 +23,38 @@ function borrar(){
 	ctx.clearRect(0,0,canvas.width,canvas.height);
 }
 
+function Rectangulo(x,y,width,height,color) {
+	this.posx=x;
+	this.posy=y;
+	this.width=width;
+	this.height=height;
+	this.color=color;
+}
+
+Rectangulo.prototype.dibujar= function(){
+	ctx.fillStyle=this.color;
+	ctx.beginPath();
+	ctx.fillRect(this.posx, this.posy, this.width, this.height);
+	ctx.closePath();
+}
+
+Rectangulo.prototype.mousedown = function(layerX,layerY){
+	var x = layerX;
+	var y = layerY;
+	if((x <= (this.posx+this.width) && x >= this.posx) && (y <= (this.posy+this.height) && y >= this.posy)){
+		this.color=colourRandom();
+		console.log("click dentro del rectangulo. El color del rectangulo cambiara");
+		if(this.width === this.height){
+			draggingCua = true;
+		}else{
+			draggingRec = true;
+		}
+	}
+	else{
+		console.log("click fuera del rectangulo");
+	}
+}
+
 function Circulo(x,y,radio,color){
 	this.posx=x;
 	this.posy=y;
@@ -36,14 +70,14 @@ Circulo.prototype.dibujar= function(){
 	ctx.closePath();
 }
 
-Circulo.prototype.mousedown = function(clientx,clienty){
-	var x = Math.pow((clientx-this.posx),2);
-	var y = Math.pow((clienty-this.posy),2);
+Circulo.prototype.mousedown = function(layerX,layerY){
+	var x = Math.pow((layerX-this.posx),2);
+	var y = Math.pow((layerY-this.posy),2);
 	var d1 = Math.sqrt(x + y);
 	if(d1 <= this.radio){
 		this.color=colourRandom();
 		console.log("click dentro del circulo. El color del circulo cambiara");
-		dragging = true;
+		draggingCir = true;
 	}
 	else{
 		console.log("click fuera del circulo");
@@ -53,22 +87,63 @@ Circulo.prototype.mousedown = function(clientx,clienty){
 var color = colourRandom();
 var x = positionRandom(canvas.width);
 var y = positionRandom(canvas.height);
+var xr = positionRandom(canvas.width);
+var yr = positionRandom(canvas.height);
+var xcua = positionRandom(canvas.width);
+var ycua = positionRandom(canvas.height);
+
 var cir = new Circulo(x,y,75,color);
+var rec = new Rectangulo(xr,yr,150,80);
+var cua = new Rectangulo(xcua,ycua,100,100);
+
+var figuras = [];
+figuras.push(cir);
+figuras.push(rec);
+figuras.push(cua);
+
 
 ctx.onload = cir.dibujar();
+ctx.onload = rec.dibujar();
+ctx.onload = rec.dibujar();
 
 canvas.onmousedown = function(event){
 	cir.mousedown(event.layerX,event.layerY);
-	if(dragging){
+	rec.mousedown(event.layerX,event.layerY);
+	cua.mousedown(event.layerX,event.layerY);
+	if(draggingCir){
 		canvas.onmousemove = function(e){
 			cir.posx=e.layerX;
 			cir.posy=e.layerY;
 			borrar();
 			cir.dibujar();
+			rec.dibujar();
+			cua.dibujar();
+		}	
+	}
+	if(draggingRec){
+		canvas.onmousemove = function(e){
+			rec.posx=e.layerX;
+			rec.posy=e.layerY;
+			borrar();
+			rec.dibujar();
+			cir.dibujar();
+			cua.dibujar();
+		}	
+	}
+	if(draggingCua){
+		canvas.onmousemove = function(e){
+			cua.posx=e.layerX;
+			cua.posy=e.layerY;
+			borrar();
+			cua.dibujar();
+			rec.dibujar();
+			cir.dibujar();
 		}	
 	}
 }
 canvas.onmouseup = function (){
-	dragging=false;
+	draggingCir=false;
+	draggingRec=false;
+	draggingCua=false;
 	canvas.onmousemove=null;
 }
