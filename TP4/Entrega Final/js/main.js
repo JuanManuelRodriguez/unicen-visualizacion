9c477e01d9d2237140bb0d2af407e75906d85f76
Juan Manuel <rodriguezjuanmanuelar@gmail.com>
2017-10-25 15:13:32 -0300

    slide del personaje

--- TP4/Entrega Final/js/main.js
+++ TP4/Entrega Final/js/main.js
@@ -1,6 +1,31 @@
 document.addEventListener('keydown', (event) => {
   const keyName = event.key;
-  if(keyName=="ArrowLeft" || keyName=="ArrowUp" || keyName=="ArrowRight" || keyName=="ArrowDown"){
-    alert('keydown event\n\n' + 'key: ' + keyName);
+  let action = document.getElementById('hero');
+  /*if(keyName=='ArrowLeft' || keyName=='ArrowUp' || keyName=='ArrowRight' || keyName=='ArrowDown'){
+    action.classList.remove('run');
+    action.classList.add('jump');
+    setTimeout(function(){
+      action.classList.remove('jump');
+      action.classList.add('run');
+    }, 1000);
+  }*/
+
+  switch(keyName){
+    case 'ArrowUp':
+      action.classList.remove('run');
+      action.classList.add('jump');
+      setTimeout(function(){
+        action.classList.remove('jump');
+        action.classList.add('run');
+      }, 1000);
+      break;
+    case 'ArrowDown':
+      action.classList.remove('run');
+      action.classList.add('slide');
+      setTimeout(function(){
+        action.classList.remove('slide');
+        action.classList.add('run');
+      }, 1000);
+      break;
   }
 });
